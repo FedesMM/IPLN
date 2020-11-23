d8a765937d451dc4834805397e815a0e2af524cc
FedesMM <fmatonte@gmail.com>
2020-11-23 17:13:00 -0300

    Quitamos  y de las features y agregamos plurales.

--- evaluate.py
+++ evaluate.py
@@ -3,16 +3,17 @@ import train
 import numpy as np
 
 def evaluate(model, t, max_length, path, csv):
-
+	print ("max_length: "+str(max_length))
 	test = files_manager.load_file(path + csv)
-	test_data = train.prepare_tweets(test, t, max_length-1)
-	y=(np.asmatrix(test_data[1])).T
-	test_data_x = np.concatenate ((test_data[0] , (np.asmatrix(test_data[1])).T), axis=1)	
+	test_data_x, test_data_y = train.prepare_tweets(test, t, max_length-1)
+	#max_length+=1
+	#y=(np.asmatrix(test_data_y)).T
+	#test_data_x = np.concatenate ((test_data[0] , (np.asmatrix(test_data[1])).T), axis=1)	
 	print(test_data_x.shape)
-	print(y.shape)
+	#print(y.shape)
 
 	# evaluate the model
-	loss, accuracy = model.evaluate(x=test_data_x, y=y, verbose=0)
+	loss, accuracy = model.evaluate(x=test_data_x, y=test_data_y, verbose=0)
 	print('Accuracy '+ csv +': %f' % (accuracy * 100))
 	predicted = model.predict(test_data_x)
 	files_manager.save_file(csv, predicted)
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from os import name
+import re
 from keras_preprocessing.sequence import pad_sequences
 from keras_preprocessing.text import one_hot, Tokenizer
 from tensorflow.python.keras import Sequential
@@ -7,16 +8,30 @@ from files_manager import load_fasttext_es_300, load_test, load_file, preprocesi
 import numpy as np
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
+'''Recibe una lista de twits y devuelve un arreglo enteros que cuenta cuantas palabras en plurar hay en cada twit de la lista de entrada.'''
+def contar_plurales(twits):
+	print ("twits.size: "+str(twits.size))
+	conteo_plurales=[]
+	for twit in twits:	
+		plurales= re.compile(r'[o|a]s(?![a-zA-Z0-9])')
+		contados=len(re.findall(plurales, str(twit)))
+		conteo_plurales+=[contados]
+	return conteo_plurales
 
 def prepare_tweets(tweets, tokenizer, max_length):
+	# Features antes del preprocesing
+	#Feature 1
+	plurales=contar_plurales(tweets)
+	print ("plurales1: "+str(plurales))
+
 	twits = preprocesing(tweets[:len(tweets), 0])
 	# define class labels
-	
 	y = tweets[:, 1].astype('float32')
-	
+
 	# integer encode the documents
 	encoded_twits = tokenizer.texts_to_sequences(twits)
 	x = pad_sequences(encoded_twits, maxlen=max_length, padding='post').astype('float32')
+	x = np.concatenate ((x , (np.asmatrix(plurales)).T), axis=1)
 	return (x,y)
 
 def train(data_path):
@@ -41,16 +56,18 @@ def train(data_path):
 	max_length = max_len
 	train_data_x, train_data_y = prepare_tweets(train, t, max_length)
 	val_data_x, val_data_y  = prepare_tweets(val, t, max_length)
+	#Se agrego la feature plurales 
+	max_length+=1
 	print("x: "+str(train_data_x.shape)+", y: "+str(train_data_y.shape))
 	print("x: "+str(val_data_x.shape)+", y: "+str(val_data_y.shape))
 
 	#calculo features
-	max_length=max_length+1
+	#max_length=max_length+1
 	
-	new_train_data_x = np.concatenate ((train_data_x , (np.asmatrix(train_data_y)).T), axis=1)
-	new_val_data_x = np.concatenate ((val_data_x , (np.asmatrix(val_data_y)).T), axis=1)	
-	print(new_train_data_x.shape)
-	print(new_val_data_x.shape)
+	#new_train_data_x = np.concatenate ((train_data_x , (np.asmatrix(train_data_y)).T), axis=1)
+	#new_val_data_x = np.concatenate ((val_data_x , (np.asmatrix(val_data_y)).T), axis=1)	
+	#print(new_train_data_x.shape)
+	#print(new_val_data_x.shape)
 
 	# load the whole embedding into memory
 	embeddings_index = dict()
@@ -87,10 +104,15 @@ def train(data_path):
 	print(model.summary())
 
 	# Set callback functions to early stop training and save the best model so far
-	callbacks = [EarlyStopping(monitor='val_accuracy', patience=5),
+	callbacks = [EarlyStopping(monitor='val_accuracy', patience=50),
 		ModelCheckpoint(filepath='best_model.h5', monitor='val_accuracy', save_best_only=True)]
+	
+	print(train_data_x.shape)
+	print(train_data_y.shape)
+	print(val_data_x.shape)
+	print(val_data_y.shape)
 	# fit the model
-	model.fit(new_train_data_x, train_data_y, epochs=100, verbose=1, callbacks=callbacks, validation_data=(new_val_data_x,val_data_y), validation_batch_size=32)
+	model.fit(train_data_x, train_data_y, epochs=100, verbose=1, callbacks=callbacks, validation_data=(val_data_x,val_data_y), validation_batch_size=32)
 	return model, t, max_length
 
 
